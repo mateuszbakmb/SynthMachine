6db2758405b0ad0e915222f8a658508c6235ca0b
mateuszbakmb <mateuszbak.mb@gmail.com>
2018-12-16 04:12:59 +0100

    Updates

---
+++ precache-manifest.fa9c2c23145f9caac3de8d235eb1fe15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a6cd0fe4dbad09e6cfc",
+    "url": "/SynthMachine/static/css/main.dd309387.chunk.css"
+  },
+  {
+    "revision": "4a6cd0fe4dbad09e6cfc",
+    "url": "/SynthMachine/static/js/main.4a6cd0fe.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/SynthMachine/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "2a8982bfc776346ee8ba",
+    "url": "/SynthMachine/static/js/runtime~main.2a8982bf.js"
+  },
+  {
+    "revision": "8b2fe2c5ea70be9b8a6773ccaa957c3c",
+    "url": "/SynthMachine/static/media/trickax.8b2fe2c5.otf"
+  },
+  {
+    "revision": "3a90e6c585f6266ba5333cb83a7617b8",
+    "url": "/SynthMachine/static/media/pdark.3a90e6c5.ttf"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/SynthMachine/static/media/digital.1e670d88.ttf"
+  },
+  {
+    "revision": "90ccdcf6b34e476f1a903ba37723094a",
+    "url": "/SynthMachine/index.html"
+  }
+];
